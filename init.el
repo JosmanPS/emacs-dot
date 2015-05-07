a7d7b5125e36158fc51170856386091f05dbe31e
Omar Trejo <otrenav@gmail.com>
2015-05-07 00:24:41 -0500

    Initial upload.

---
+++ init.el
@@ -0,0 +1,21 @@
+;;
+;; Emacs configuration file
+;; @author: Omar Trejo
+;;
+
+(load "~/.emacs.d/path.el")
+(load "~/.emacs.d/packages.el")
+(load "~/.emacs.d/functions.el")
+(load "~/.emacs.d/settings.el")
+(load "~/.emacs.d/editing.el")
+(load "~/.emacs.d/org-mode.el")
+(load "~/.emacs.d/web.el")
+(load "~/.emacs.d/terminal.el")
+(load "~/.emacs.d/latex.el")
+(load "~/.emacs.d/helm.el")
+(load "~/.emacs.d/spellcheck.el")
+(load "~/.emacs.d/python.el")
+(load "~/.emacs.d/matlab.el")
+(load "~/.emacs.d/ess.el")
+(load "~/.emacs.d/lisp.el")
+(load "~/.emacs.d/keybindings.el")
